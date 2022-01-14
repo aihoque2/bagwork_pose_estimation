8bffa46a18278bc225136c5071313eeaea0ec2a7
aihoque2 <aihoque2@illinois.edu>
2022-01-13 20:43:26 -0600

    added new functions and our data

--- get_video.py
+++ get_video.py
@@ -2,5 +2,12 @@
 import cv2
 import torch
 
+def get_image_from_video(filename, index):
+    """
+    read the video with cv2 then select
+    the frame the the particular index
+    """
+    pass
+
 def get_video(filename):
     pass
