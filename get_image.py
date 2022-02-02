85f757e6c2c9692beb1c0dab04a224ccc888f3ba
aihoque2 <aihoque2@illinois.edu>
2022-02-01 20:59:47 -0600

    learned how to use opencv properly

--- get_image.py
+++ get_image.py
@@ -10,9 +10,14 @@ def get_image_from_video(filename, index):
     return the mat that represents the frame
     """
     cap = cv2.VideoCapture(filename)
+
+    ##NOTE: CAPTURING VIDEO FRAMES:
+    ##in order to capture a video frame,
+
     i = 0
-    while (i <= index):
-        frame = cap.read()
+    success = 1
+    while (success and i <= index):
+        success, frame = cap.read()
         if (i==index):
             return frame
         i+=1
--- main.py
+++ main.py
@@ -9,8 +9,10 @@ as seen here:
 https://arxiv.org/pdf/2008.09457.pdf
 """
 import cv2
-from get_video import get_image_from_video
+from get_image import get_image_from_video
 
 filename = "video/jab.mp4"
-mat = get_image_from_video(filename, 0)
+mat = get_image_from_video(filename, 10)
+print("here's mat type: ")
+print(type(mat))
 cv2.imwrite("standing.jpg", mat)
