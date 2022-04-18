37365e26295fb57d1e50e943d46914caaf52e0de
aihoque2 <aihoque2@illinois.edu>
2022-04-17 20:04:06 -0500

    dope.py is inefficient runtime. gonna fix it

--- video_processor.py
+++ video_processor.py
@@ -14,10 +14,23 @@ from torchvision.transforms import ToTensor
 
 import cv2
 
-def run_dope_on_mat(img):
+def load_mode(modelname):
+    """
+    load which model to use for psoe estimation given the name
+    """
+    pass
+
+def run_dope_on_mat(image, model, postprocessing="ppi"):
     """
     TODO: run a simple pose estimation with the given model.
+    this function is like dope() in dope.py, but we add another
+    dimension 
+
+    this assumes model has been loaded
     """
+    pass
+
+
 
 def process_video(filename, video):
     """
