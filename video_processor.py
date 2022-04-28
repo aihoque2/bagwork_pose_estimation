9402aa45837fa3002ff2effd750e6886029868ec
aihoque2 <aihoque2@illinois.edu>
2022-04-28 02:20:50 -0500

    load_model() implemented

--- video_processor.py
+++ video_processor.py
@@ -9,16 +9,37 @@ from PIL import Image
 import cv2
 import numpy as np
 
+from ai_code.model import dope_resnet50, num_joints
+
 import torch
 from torchvision.transforms import ToTensor
 
 import cv2
 
+_thisdir = osp.realpath(osp.dirname(__file__)) #TODO: wtf this do?
+
+
 def load_model(modelname):
     """
     load which model to use for psoe estimation given the name
     """
-    pass
+    device = 'cuda:0' if torch.cuda.is_available() else 'cpi'
+
+    cpkft_fname = osp.join("/ai_code/models/" + modelname)
+    
+    #error checking case
+    if not os.path.isfile(ckpt_fname):
+        raise Exception('{:s} does not exist, please download the model first and place it in the models/ folder'.format(ckpt_fname))
+    
+    ckpt = torch.load(cpkft_fname, map_location=device)
+    ckpt['dope_kwargs']['rpn_post_nms_top_n_test'] = 1000
+    model = dope_resnet50(**ckpt['dope_kwargs'])
+    if ckpt['half']: model = model.half()
+    model = model.eval()
+    model.load_state_dict(ckpt['state_dict'])
+    model = model.to(device)
+
+    return model #remember objects get returned by reference
 
 def run_dope_on_mat(image, model, postprocessing="ppi"):
     """
